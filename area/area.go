71a16c8ad998dcc09b980bbbe2a7e9dd4989c254
Abbas K T <abbas@unmarshal.io>
2021-08-19 13:18:59 +0530

    added more code for area

--- area/area.go
+++ area/area.go
@@ -3,3 +3,11 @@ package area
 func RectangleArea(w int, h int) int{
 	return w*h
 }
+
+func RectanglePerimeter(w int, h int) int{
+	return 2*(w+h)
+}
+
+func SumPerimeterArea(w,h int) int{
+	return RectangleArea(w,h)+RectanglePerimeter(w,h)
+}
--- area/rectangle_test.go
+++ area/rectangle_test.go
@@ -6,18 +6,55 @@ import (
 )
 
 func TestRectangleArea(t *testing.T) {
-	t.Run("Should return area", func( *testing.T) {
+	t.Run("Should return 20 as area when width is 4 and height is 5", func( *testing.T) {
 		got := RectangleArea(4,5)
 		want := 20
+		if got != want {
+			t.Fail()
+			t.Error(fmt.Sprintf("Expected %v got %v ",want,got))
+		}
+	})
+	t.Run("Should return 60 as area when width is 10 and height is 6", func( *testing.T) {
+		got := RectangleArea(10,6)
+		want := 60
+		if got != want {
+			t.Fail()
+			t.Error(fmt.Sprintf("Expected %v got %v ",want,got))
+		}
+	})
+}
+
+func TestRectanglePerimeter(t *testing.T) {
+	t.Run("Should return 18 as perimeter when width is 4 and height is 5", func( *testing.T) {
+		got := RectanglePerimeter(4,5)
+		want := 18
 		if got != want {
 			t.Error(fmt.Sprintf("Expected %v got %v ",want,got))
 		}
 	})
-	t.Run("Should return area", func( *testing.T) {
-		got := RectangleArea(5,6)
+	t.Run("Should return 18 as perimeter when width is 4 and height is 5", func( *testing.T) {
+		got := RectanglePerimeter(10,5)
 		want := 30
 		if got != want {
 			t.Error(fmt.Sprintf("Expected %v got %v ",want,got))
 		}
 	})
 }
+
+func TestSumPerimeterArea(t *testing.T) {
+	t.Run("Expecting 38 for width 4 and height 5", func( *testing.T) {
+		got := SumPerimeterArea(4,5)
+		want := 38
+		if got != want {
+			t.Error(fmt.Sprintf("Expected %v got %v ",want,got))
+		}
+	})
+	t.Run("Expecting 80 for width 10 and height 5", func( *testing.T) {
+		got := SumPerimeterArea(10,5)
+		want := 80
+		if got != want {
+			t.Error(fmt.Sprintf("Expected %v got %v ",want,got))
+		}
+	})
+}
+
